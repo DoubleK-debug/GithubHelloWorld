a85b1ca00f2373c6d9ee477948a4748b46e23494
Kaniel <Kaniel.kusi.2022@student.lhschools.org>
2020-02-13 12:19:18 -0500

    printedNormalHelloWorld

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+     System.out.println("Hello World");
+    }
+
+    public static void helloWorld(){ System.out.println("Hello World"); }
+
+
+
+}
